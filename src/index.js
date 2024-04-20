78dc6b73ce92d26d945b85c049f9af689e35aaee
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-04-21 02:15:31 +0300

    Completes Lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
-}
-
-document.addEventListener('DOMContentLoaded', init);
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    selectedMovies = form.searchByID.value;
+    // the input should be captured after the prevent default so it is not locked
+    fetch(`http://localhost:3000/movies/${selectedMovies}`)
+      // notice the use of backticks as you are interpolating here
+      .then((res) => res.json())
+      .then((movies) => {
+        document.querySelector("h4").innerText = movies.title;
+        document.querySelector("p").innerText = movies.summary;
+        form.reset();
+      });
+  });
+};
+document.addEventListener("DOMContentLoaded", init);
